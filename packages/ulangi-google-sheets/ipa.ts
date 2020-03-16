260b694d221cd9c73d18142b75f88898c47ef5c9
minhloi <jimmyloi92@yahoo.com>
2020-03-15 17:00:43 -0700

    feat: add ipa(), pinyin(), romaji() to Google Sheets

---
+++ packages/ulangi-google-sheets/ipa.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Minh Loi.
+ *
+ * This file is part of Ulangi which is released under GPL v3.0.
+ * See LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+import { getApiUrl } from "./getApiUrl"
+import { getApiKey } from "./getApiKey"
+import { queryStringify } from "./queryStringify"
+import { DictionaryEntry } from "@ulangi/ulangi-common/interfaces"
+
+export function ipa(term: string, languageCode: string): string {
+  const apiUrl = getApiUrl()
+  const apiKey = getApiKey()
+
+  const response = UrlFetchApp.fetch(apiUrl + '/get-dictionary-entry?' + queryStringify({
+    searchTerm: term,
+    searchTermLanguageCode: languageCode,
+    translatedToLanguageCode: "en"
+  }),
+  {
+    method: "get",
+    headers: {
+      Authorization: "Bearer " + apiKey
+    },
+    muteHttpExceptions: true
+  })
+
+  if (response.getResponseCode() === 200) {
+    const { dictionaryEntry } : { dictionaryEntry: DictionaryEntry } = JSON.parse(response.getContentText())
+
+    if (typeof dictionaryEntry.ipa !== 'undefined'){
+      return dictionaryEntry.ipa.join("\n")
+    } else {
+      return ''
+    }
+  }
+  else if (response.getResponseCode() === 401) {
+    throw new Error("The API key is invalid or expired.")
+  }
+  else {
+    throw new Error(response.getContentText())
+  }
+}
